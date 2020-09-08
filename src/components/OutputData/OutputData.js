7562b6a3d4130e98bcf03edaebdca87f2138abae
xxx <56048517+laptevlife@users.noreply.github.com>
2020-09-08 23:22:40 +0400

    Поправлен блок OUT

--- src/components/OutputData/OutputData.js
+++ src/components/OutputData/OutputData.js
@@ -8,24 +8,41 @@ export default class OutputData extends React.Component {
             <div>
                 <div className="card ">
 
+                    <div className="flex">
+                        <div className="center out left">
+                            <div className="line">
+                                <div className="headline">Общая сумма : </div>
+                                {/* <input className='input' type="text" value = {this.props.data.totalSum} readOnly /> */}
+                            </div>
+                            <div className="line">
+                                <div className="headline">Ежемесячная плата :   </div>
+                                {/* <input className='input' type="text" value = {this.props.data.monthPayment} readOnly/> */}
+                            </div>
+                            <div className="line">
+                                <div className="headline">Общая переплата :</div>
+                                {/* <input className='input' type="text" value = {this.props.data.overPayment} readOnly /> */}
+                            </div>
+                        </div>
+                        <div className="center out">
+                            <div className="line">
+                                <div className="headline">{this.props.data.totalSum}</div>
+                                {/* <input className='input' type="text" value = {this.props.data.totalSum} readOnly /> */}
+                            </div>
+                            <div className="line">
+                                <div className="headline">{this.props.data.monthPayment} </div>
+                                {/* <input className='input' type="text" value = {this.props.data.monthPayment} readOnly/> */}
+                            </div>
+                            <div className="line">
+                                <div className="headline">{this.props.data.overPayment}</div>
+                                {/* <input className='input' type="text" value = {this.props.data.overPayment} readOnly /> */}
+                            </div>
 
-                    <div className="center ">
-                        <div className="line">
-                            <div className="headline">Общая сумма : {this.props.data.totalSum}</div>
-                            {/* <input className='input' type="text" value = {this.props.data.totalSum} readOnly /> */}
-                        </div>
-                        <div className="line">
-                            <div className="headline">Ежемесячная плата :  {this.props.data.monthPayment} </div>
-                            {/* <input className='input' type="text" value = {this.props.data.monthPayment} readOnly/> */}
-                        </div>
-                        <div className="line">
-                            <div className="headline">Общая переплата : {this.props.data.overPayment}</div>
-                            {/* <input className='input' type="text" value = {this.props.data.overPayment} readOnly /> */}
-                        </div>
 
+                        </div>
 
                     </div>
-                    
+
+
                 </div>
             </div>
         )
