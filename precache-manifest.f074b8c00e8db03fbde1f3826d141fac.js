c47e267b213a64c74a2e0ca60f3db22f698a2168
xxx <56048517+laptevlife@users.noreply.github.com>
2020-10-11 21:49:41 +0400

    Updates

---
+++ precache-manifest.f074b8c00e8db03fbde1f3826d141fac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587698422c01c37e751e61b9121f9694",
+    "url": "/React-credit-calculator/index.html"
+  },
+  {
+    "revision": "3e58443d8321efddbe77",
+    "url": "/React-credit-calculator/static/css/main.f473b38a.chunk.css"
+  },
+  {
+    "revision": "2ccc3f548d151b11a519",
+    "url": "/React-credit-calculator/static/js/2.36e095e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-credit-calculator/static/js/2.36e095e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e58443d8321efddbe77",
+    "url": "/React-credit-calculator/static/js/main.09a3e9b8.chunk.js"
+  },
+  {
+    "revision": "f150bf9812d74d17e879",
+    "url": "/React-credit-calculator/static/js/runtime-main.758c9686.js"
+  }
+]);
